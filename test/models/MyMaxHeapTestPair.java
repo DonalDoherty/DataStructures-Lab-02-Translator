967f07754079d58834b1e06c20258190aa2f86f2
Donal Doherty <dohertydonal98@gmail.com>
2017-02-13 12:55:36 +0000

    Began Implementing WordPair Test For Heap

--- test/models/MyMaxHeapTestPair.java
+++ test/models/MyMaxHeapTestPair.java
@@ -1,5 +1,19 @@
 package models;
 
+import org.junit.Before;
+
+import models.WordPair;
+import utils.CSVLoader;
+
 public class MyMaxHeapTestPair {
 
+	MyMaxHeap<WordPair> pairs;
+	CSVLoader loader;
+	
+	@Before
+	public void setUp(){
+		pairs = loader.loadPairs("data/pairs.txt");
+	}
+	
+	
 }
